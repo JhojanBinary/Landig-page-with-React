987d2bdc307744a2196500c840f01c91589cfa5d
JhojanBinary <102551448+JhojanBinary@users.noreply.github.com>
2024-04-25 18:41:41 -0500

    Card.js

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -4,12 +4,12 @@ export function Card() {
 
     return (
         <div className="">
-            <div class="carta">
-                <img src="https://th.bing.com/th/id/OIP.uN-BNWdw2YIyEZsMwzDVWwHaHN?rs=1&pid=ImgDetMain" class="card-img-top" alt="..."/>
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text"> thSome quick example text to build on the card title and make up th Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somhere</a>
+            <div className="carta">
+                <img src="https://th.bing.com/th/id/OIP.uN-BNWdw2YIyEZsMwzDVWwHaHN?rs=1&pid=ImgDetMain" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text"> thSome quick example text to build on the card title and make up th Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go somhere</a>
                     </div>
             </div>
 
